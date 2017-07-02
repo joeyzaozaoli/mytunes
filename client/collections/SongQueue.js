9137624da03c0788286364689ae11cb686b5ead1
Joey Li <joeyzaozaoli@gmail.com>
2017-07-01 19:44:42 -0700

    Pass SongQueue tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,30 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function() {
+      this.removeSong(this.at(0));
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function() {
+      this.removeSong(this.at(0));
+    }, this);
+  },
+
+  playFirst: function() {
+    this.first().play();
+  },
+
+  removeSong: function(song) {
+    this.remove(song);
   }
 
 });
