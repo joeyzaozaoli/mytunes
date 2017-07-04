d18fdce14c2d1e384809b85fdbbb2f1a37db84e7
Joey Li <joeyzaozaoli@gmail.com>
2017-07-03 19:25:34 -0700

    Pass Songs tests

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,21 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function() {
+    var c = this;
+    $.ajax({
+      url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
+      type: 'GET',
+      success: function (data) {
+        console.log('get success');
+        c.add(data.results);
+      },
+      error: function (data) {
+        console.error('get failure', data);
+      }
+    });
+  }
 
 });
